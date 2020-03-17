c672c8e5bc5365d6fa3409e7bdae32a99ba5e483
Seppel3210 <seppel3210@gmail.com>
2020-03-17 13:30:11 +0100

    Change key for cw-rotations from D to S

--- src/InputHandler.kt
+++ src/InputHandler.kt
@@ -13,7 +13,7 @@ class InputHandler : KeyAdapter() {
         when (e.keyCode) {
             KeyEvent.VK_LEFT -> left = true
             KeyEvent.VK_RIGHT -> right = true
-            KeyEvent.VK_D -> cw = true
+            KeyEvent.VK_S -> cw = true
             KeyEvent.VK_A -> ccw = true
             KeyEvent.VK_DOWN -> softDrop = true
             KeyEvent.VK_SPACE -> hardDrop = true
@@ -26,7 +26,7 @@ class InputHandler : KeyAdapter() {
         when (e.keyCode) {
             KeyEvent.VK_LEFT -> left = false
             KeyEvent.VK_RIGHT -> right = false
-            KeyEvent.VK_D -> cw = false
+            KeyEvent.VK_S -> cw = false
             KeyEvent.VK_A -> ccw = false
             KeyEvent.VK_DOWN -> softDrop = false
             KeyEvent.VK_SPACE -> hardDrop = false
