f2849c556e41dd18250ccc515991a74fac25e063
Seppel3210 <seppel3210@gmail.com>
2020-03-17 13:27:55 +0100

    Set JFrame Default_Close_Operation to EXIT_ON_CLOSE

--- src/gui/Window.kt
+++ src/gui/Window.kt
@@ -2,9 +2,11 @@ package gui
 
 import java.awt.Component
 import javax.swing.JFrame
+import javax.swing.WindowConstants
 
 class Window(component: Component) : JFrame("Kotris") {
     init {
+        defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
         add(component)
         pack()
         setLocationRelativeTo(null)
